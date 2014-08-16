c6f281712d7adccf3eb980445d313c472a672894
Jie Feng <jiefeng.hopkins@gmail.com>
2014-08-16 16:29:29 -0700

    coffee makes you feel good

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Engex::Application.config.secret_key_base = '7e62a2535ae0e19953e83f2c1ee45606aac69d78d6cdcc9396ab90f37671fc6a577cdf4c4ab68c45f2ae8d77800c96801d4531f1871466557fc114c76417885a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Engex::Application.config.secret_key_base = secure_token
+#Engex::Application.config.secret_key_base = '7e62a2535ae0e19953e83f2c1ee45606aac69d78d6cdcc9396ab90f37671fc6a577cdf4c4ab68c45f2ae8d77800c96801d4531f1871466557fc114c76417885a'
