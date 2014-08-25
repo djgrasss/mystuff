749bf56cc223eb791313aca45d32f5541200039d
Jie Feng <jiefeng.hopkins@gmail.com>
2014-08-24 22:51:30 -0700

    added username auto generation into User model

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,6 +15,7 @@ describe "Authentication" do
       before { click_button "Sign in" }
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-danger') }
+      it { should_not have_link('Settings',    href: edit_user_path(user)) }
       describe "after visiting another page" do
         before {click_link "Home"}
         it { should_not have_selector('div.alert.alert-danger') }
@@ -31,6 +32,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
