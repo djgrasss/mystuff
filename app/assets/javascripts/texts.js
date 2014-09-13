e7d94b38d5fd2d92a33a98d3578e6429c86be6f2
Jie Feng <jiefeng.hopkins@gmail.com>
2014-09-12 23:54:12 -0400

    text css fancied

--- app/assets/javascripts/texts.js
+++ app/assets/javascripts/texts.js
@@ -27,36 +27,7 @@
                                 clearInterval(erupt);
                         };
                         var erupt=setInterval(eruption, 500);
-
-                        $('.txt_wrap').each(function(){
-                            var $wrap=$(this);
-                            var $txt=$wrap.find(".selection_text");
-                            var wrap_height=parseInt($wrap.innerHeight(),10);
-                            var txt_height=parseInt($txt[0].scrollHeight,10);
-                            if (txt_height>wrap_height) {
-                                var $more=$wrap.parent().find('.more');
-                                $more.click(function(){
-                                    if ($wrap.hasClass('height_limit')) {
-                                        $wrap.removeClass('height_limit');
-                                        var $span=$more.find('span');
-                                        $span.html("Less");
-                                        $span.removeClass('glyphicon-arrow-down');
-                                        $span.addClass('glyphicon-arrow-up');
-                                        $('.item').resize();
-                                    } else {
-                                        $wrap.addClass('height_limit');
-                                        var $span=$more.find('span');
-                                        $span.html("More");
-                                        $span.removeClass('glyphicon-arrow-up');
-                                        $span.addClass('glyphicon-arrow-down');
-                                        $('.item').resize();
-                                    }
-                                });
-                                $more.show();
-
-                            }
-
-                        });
+                        txt_wrap();
                     },
                     renderData: function (data, dataType) {
                         var tpl,template;
