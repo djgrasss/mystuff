12781537ebd881d1930e359f0d0c6f97f261a258
Jie Feng <jiefeng.hopkins@gmail.com>
2014-09-30 00:05:30 -0400

    yes we can

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,5 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var v;
 $(document).ready(function(){
     $('#calendar').fullCalendar({
         header: {
@@ -10,12 +11,25 @@ $(document).ready(function(){
         selectable: true,
         selectHelper: true,
         select: function(start, end) {
-
         },
         editable: true,
         eventLimit: true,
         height:500,
-        events: "events"
-    });
+        events: "events",
+        dayClick: function(date, allDay, jsEvent, view) {
+            // change the day's background color just for fun
+            $('#calendar').fullCalendar('changeView', 'agendaDay');
+            $('#calendar').fullCalendar('gotoDate', date);
+        },
 
+        eventDrop: function(event, delta, revertFunc) {
+            console.log(event);
+            console.log(delta);
+            alert(event.title + " was dropped on " + event.start.format());
+            if (!confirm("Are you sure about this change?")) {
+                revertFunc();
+            }
+        }
+
+    });
 });
