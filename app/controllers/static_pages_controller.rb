69457a0ab5ca7f37653af00792bcd715782c31aa
Jie Feng <jiefeng.hopkins@gmail.com>
2014-10-10 23:51:23 -0400

    added peng, and styled team

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,44 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    @team = [
+        {image:"team/photo-jiefeng.png",
+         name: "Jie Feng",
+         role: "Co-Founder & Chairman",
+         email: "jiefeng.hopkins@gmail.com",
+         education: "The Johns Hopkins University",
+         intro: "Tech engine, Management"
+        },
+        {image:"team/pengjianxiang.jpg",
+         name: "Jianxiang Peng",
+         role: "Co-Founder",
+         email: "pjx911@gmail.com",
+         education: "The Johns Hopkins University",
+         intro: "Backend, Android & IOS"
+        },
+        {image:"team/photo-scottzhang.png",
+         name: "Scott Zhang",
+         role: "Co-Founder",
+         email: "scott.likai.zhang@gmail.com",
+         education: "The Johns Hopkins University",
+         intro: "Frontend, Backend"
+        },
+        {image:"team/photo-chenzhenwu.jpg",
+         name: "Chenzhen Wu",
+         role: "Co-Founder",
+         email: "chzhen.wu@hotmail.com",
+         education: "École Polytechnique Fédérale de Lausanne",
+         intro: "Backend, Strategy"
+        },
+        {image:"team/photo-chunxiao.jgp",
+         name: "Chunxiao Song",
+         role: "Co-Founder",
+         email: "songchunxiao.jhu@gmail.com",
+         education: "The Johns Hopkins University",
+         department: "Bloomberg School of Public Health",
+         intro: "Design, Marketing"
+        }
+    ]
   end
 
   def contact
